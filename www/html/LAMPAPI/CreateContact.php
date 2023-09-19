e7ac0bf373afb32d7ec233ba45beee4fd517beb0
melanieviolett <melanieviolettt@gmail.com>
2023-09-18 22:23:45 -0400

    Update CreateContact.php - add avatar on create contact

--- www/html/LAMPAPI/CreateContact.php
+++ www/html/LAMPAPI/CreateContact.php
@@ -1,5 +1,5 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json; charset=UTF-8');
   header('Access-Control-Allow-Methods: GET,POST,PUT,PATCH,DELETE');
   header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With');
@@ -9,71 +9,71 @@
     exit();
   }
 
-
-	$inData = getRequestInfo();
- 
- 
-  $curr_ID = $inData['UserID'];
-	$firstName = $inData['FirstName'];
-	$lastName = $inData['LastName'];
-  $email = $inData['Email'];
-  $phoneNumber = $inData['Phone'];
-	$date = date('Y-m-d H:i:s');
+        $inData = getRequestInfo();
 
 
-	$conn = mysqli_connect("localhost", "AdminAccount", "wearetesting", "COP4331");
-	if (!$conn)
-	{
-		die("Connection failed: " . mysqli_connect_error());
-	}
+  	$curr_ID = $inData['UserID'];
+        $firstName = $inData['FirstName'];
+        $lastName = $inData['LastName'];
+  	$email = $inData['Email'];
+        $phoneNumber = $inData['Phone'];
+        $avatar = $inData['Avatar'];
+        $date = date('Y-m-d H:i:s');
 
-	else
-	{
+
+        $conn = mysqli_connect("localhost", "AdminAccount", "wearetesting", "COP4331");
+        if (!$conn)
+        {
+                die("Connection failed: " . mysqli_connect_error());
+        }
+
+        else
+        {
     $lastLogged = $conn->prepare("SELECT * from Users WHERE ID = ?");
-		$lastLogged->bind_param("i", $curr_ID);
+                $lastLogged->bind_param("i", $curr_ID);
     $currLastLogged = "";
-    
-		if($lastLogged->execute())
-		{
+
+                if($lastLogged->execute())
+                {
         $curr_user = mysqli_fetch_assoc(mysqli_stmt_get_result($lastLogged));
         $currLastLogged = $curr_user['DateLastLoggedIn'];
-		}
+                }
 
-		$stmt = $conn->prepare("INSERT into Contacts (DateCreated, DateLastLoggedIn, FirstName,LastName,Phone,Email,UserID) VALUES ('$date','$currLastLogged','$firstName','$lastName','$phoneNumber','$email','$curr_ID')");
-    if($stmt->execute()) 
+                $stmt = $conn->prepare("INSERT into Contacts (DateCreated, DateLastLoggedIn, FirstName,LastName,Phone,Email,UserID,Avatar) VALUES ('$date','$currLastLogged','$firstName','$lastName','$phoneNumber','$email','$curr_ID','$avatar')");
+    if($stmt->execute())
     {
         returnWithInfo($firstName, $lastName, "Success!");
     }
-		else
-		{
-			returnWithError("Please fill out all fields!");
-		}
+                else
+                {
+                        returnWithError("Please fill out all fields!");
+                }
 
-		if($lastLogged != NULL){$lastLogged->close();}
-		if($conn != NULL){$conn->close();}
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+                if($lastLogged != NULL){$lastLogged->close();}
+                if($conn != NULL){$conn->close();}
+        }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"returnID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"returnID":100,"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"returnID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+
+        function returnWithInfo( $firstName, $lastName, $id )
+        {
+                $retValue = '{"returnID":100,"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
+                sendResultInfoAsJson( $retValue );
+        }
+
 ?>
